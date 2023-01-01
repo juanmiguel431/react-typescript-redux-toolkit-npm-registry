d35ec0d30e1242264095f5a4426eebcf6f097858
juanmiguel431 <juanmiguel431@gmail.com>
2022-12-31 19:38:29 -0500

    unused code removed.

--- src/state/action-types/index.ts
+++
@@ -1,5 +0,0 @@
-export enum ActionType {
-  SearchRepositories = 'repositories/search',
-  SearchRepositoriesSuccess = 'repositories/searchSuccess',
-  SearchRepositoriesError = 'repositories/searchError',
-}
