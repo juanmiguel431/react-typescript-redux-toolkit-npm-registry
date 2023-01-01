893d61f62ffd84ddaea80144e29ae8b4d4ad90ba
juanmiguel431 <juanmiguel431@gmail.com>
2022-12-31 19:52:34 -0500

    Typing registry.npmjs.org result

--- src/state/reducers/RepositoriesReducer.ts
+++ src/state/reducers/RepositoriesReducer.ts
@@ -13,16 +13,26 @@ const initialState: RepositoriesState = {
   data: []
 };
 
+interface Response {
+  objects: [
+    {
+      package: {
+        name: string;
+      }
+    }
+  ]
+}
+
 export const searchByTerm = createAsyncThunk<string[], string>(
   'repositories/searchByTerm',
   async (term): Promise<string[]> => {
-    const { data } = await axios.get('https://registry.npmjs.org/-/v1/search', {
+    const { data }: { data: Response } = await axios.get('https://registry.npmjs.org/-/v1/search', {
       params: {
         text: term
       }
     });
 
-    return data.objects.map((p: any) => {
+    return data.objects.map((p) => {
       return p.package.name;
     });
   }
