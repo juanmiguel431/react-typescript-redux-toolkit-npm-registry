d132814fafdfe7e7e4c97a50659c3314052317d3
juanmiguel431 <juanmiguel431@gmail.com>
2023-01-02 23:06:05 -0500

    RepositoriesList updated.

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { useState } from 'react';
 
 export const RepositoriesList: React.FC = () => {
+  const [term, setTerm] = useState('');
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+    console.log('JMPC');
+  };
+
   return (
     <div className="repositories-list">
-      <input type="text"/>
-      <button>Search</button>
+      <form onSubmit={onSubmit}>
+        <input type="text" onChange={e => setTerm(e.target.value)} value={term}/>
+        <button>Search</button>
+      </form>
     </div>
   );
 };
